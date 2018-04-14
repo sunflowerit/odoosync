9c6ff4498301e1f21a357d68ff6072bb3d5f5521
hayyan <hayyan-ebrahem@hotmail.com>
2018-04-14 14:15:37 +0200

    sync customer_asset to portal

--- ModelSyncer.py
+++ ModelSyncer.py
@@ -1,6 +1,7 @@
 from collections import OrderedDict, defaultdict
 from pprint import pprint
 
+
 class ModelSyncer():
 
     def __init__(self, source, dest):
@@ -8,13 +9,11 @@ class ModelSyncer():
         self.dest = dest
         self.source_ir_fields = source.env['ir.model.fields']
         self.dest_ir_model_obj = dest.env['ir.model.data']
-        # self._main_odoo_excluded_models = ('mail.followers','account_analytic_analysis.summary.user','account_analytic_analysis.summary.month','account.fiscal.position','account.account','account.invoice','account.analytic.account','account.analytic.line','product.pricelist','crm.case.section','res.country','product.uom','resource.calendar','res.currency','ir.actions.actions','res.country.state','ir.model','res.company',)
         self._depends_struct = OrderedDict()
         self._fields_struct = {}
         self._many2one_fields = defaultdict(list)
         self._one2many_fields = defaultdict(list)
         self._prefix = '__export_sfit__.'
-
 
     def _prepare_sync(self, model,domain,excluded_fields):
         source_records = []
@@ -33,13 +32,12 @@ class ModelSyncer():
                 if field.get('relation') not in excluded_fields:
                     if field.get('ttype') == 'many2one':
                         self._many2one_fields[field.get('relation')].append(field.get('name'))
-                    elif field.get('ttype') in ('one2many','many2many'):
+                    elif field.get('ttype') in ('one2many', 'many2many'):
                         self._one2many_fields[field.get('relation')].append(field.get('name'))
-        print ' model is ', model
-        print ' excluded fields ', excluded_fields
-        self._fields_struct.update(dict({model:  model_fields}))
+
+        self._fields_struct.update(dict({model: model_fields}))
         for record in source_records:
-            field_data = map(lambda x: record[x], self._fields_struct.get(model))
+            field_data = map(lambda x: record.get(x), self._fields_struct.get(model))
             self.source_trans.update({record['id']: field_data})
 
         dest_external_ids = self.dest_ir_model_obj.search([('name', 'ilike', self._prefix+'%')])
@@ -70,41 +68,42 @@ class ModelSyncer():
         for field,value in vals.items():
             if value:
                 if field in many2one_fields:
-                    for k,v in self._many2one_fields.iteritems():
+                    for k, v in self._many2one_fields.iteritems():
                         if field in v:
                             # vals[k] = vals.pop(field)
-                            proper_name.append([field,self._get_proper_name(k, value[0])])
+                            proper_name.append([field,k, value[0]])
 
                 elif field in one2many_fields:
                     for k,v in self._one2many_fields.iteritems():
                         if field in v:
                             # vals[k] = vals.pop(field)
                             for i in value:
-                                proper_name.append([field,self._get_proper_name(k, i)])
+                                proper_name.append([field,k, i])
                         # vals[k] =  [(0, 0, {})]
 
             # else:
             #     print ' consider related models for these fields ', field
         # pprint(proper_name)
-        for name in proper_name:
-            dest_id = self.dest_ir_model_obj.search([('name', '=', self._prefix+name[1])])
+        for record in proper_name:
+            field, model_name, proper_name = record[0], record[1], self._get_proper_name(record[1],record[2])
+            dest_id = self.dest_ir_model_obj.search([('name', '=', self._prefix+proper_name)])
             if dest_id:
                 dest_id = self.dest_ir_model_obj.read(dest_id, ['res_id', 'model'])
                 # related_model = dest_id[0].get('model')
                 field_id = dest_id[0].get('res_id')
-                if name[0] in many2one_fields:
-                    vals.update({name[0]: field_id})
-                elif name[0] in one2many_fields:
-                    _id = int(name[1].split('_')[-1])
-                    _id_index = vals[name[0]].index(_id)
-                    vals[name[0]][_id_index] = field_id
+                if field in many2one_fields:
+                    vals.update({field: field_id})
+                elif field in one2many_fields:
+                    _id = int(proper_name.split('_')[-1])
+                    _id_index = vals[field].index(_id)
+                    vals[field][_id_index] = field_id
             else:
-                print ' name in prop_name doesn\'t exist', name
-        print ' vals after for model ', model
-        pprint(vals)
+                print '----------------- consider syncing model ', model_name,' with id:', record[2]
+                # self._sync_one_model(model_name,domain=[],excluded_fields=['ir.model','mail.alias','mail.alias','ir.model.fields'])
         return vals
 
-    def _sync_one_model(self, model):
+    def _sync_one_model(self, model,domain,excluded_fields):
+        self._prepare_sync(model,domain,excluded_fields)
         if self.source_trans:
             print 'syncing model  model ', model
             for id, data in self.source_trans.iteritems():
@@ -125,8 +124,7 @@ class ModelSyncer():
                     self.dest_ir_model_obj.create(external_ids)
 
     def sync(self, model):
-        domain = model.get('domain')
-        excluded_fields = model.get('excluded_fields')
+        domain = model.get('domain')[0]
+        excluded_fields = model.get('excluded_fields')[0]
         model_to_sync = model.get('model')
-        self._prepare_sync(model_to_sync,domain[0],excluded_fields[0])
-        self._sync_one_model(model_to_sync)
+        self._sync_one_model(model_to_sync,domain,excluded_fields)
