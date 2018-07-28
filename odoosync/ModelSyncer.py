c15b917f5503d8c9c2a8765df1c5ede75350d0da
odoo8@freshapp02test <antiflu@gmail.com>
2018-07-28 19:04:16 +0200

    fixup! [IMP] Python logging, and dont update dependent records (only create)

--- odoosync/ModelSyncer.py
+++ odoosync/ModelSyncer.py
@@ -10,7 +10,7 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 ch = logging.StreamHandler(sys.stdout)
 ch.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
